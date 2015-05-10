ff41e905616459f438f19cbe4a59e041780fc7f7
Savic123 <chaos2001>
2015-05-11 00:31:24 +0200

    added database conn

--- demo/server.js
+++ demo/server.js
@@ -4,11 +4,30 @@
  
 var express = require('express');
 var bodyParser = require('body-parser');
+var pg = require("pg");
+
+var dataBaseConnectionString = "pg://ubuntu@localhost:5432/circle_test";
+var client = new pg.Client(dataBaseConnectionString);
+
 
 var app = express();
 // parse application/json
 app.use(bodyParser.json());
 
+// connect to databsae
+client.connect(function(err) {
+	if (err) {
+		return console.log('could not connect to postgres', err);
+	}
+
+	// log successful database connection
+	client.query('SELECT NOW() AS "theTime"', function(err, result) {
+		if (err) {
+			return console.error('error running query', err);
+		}
+		console.log('Successfully connected to the db on ' + result.rows[0].theTime);
+	});
+});
 
 app.get('/', function (req, res) {
 	
